f2610dd937cd3275bba046cb44a227b48eb3c066
tata-ost17.github.io <63121337+tata-ost17@users.noreply.github.com>
2021-02-26 19:13:37 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -42,11 +42,11 @@ $(function(){
    });
 
    $('.gallery__slider-sm').slick({
-      slidesToShow: 4,
+      slidesToShow: 6,
       slidesToScroll: 1,
       arrows: false,
-      centerMode: false,
-      focusOnSelect: true,
+      //centerMode: true,
+      //focusOnSelect: true,
       variableWidth: true, 
       asNavFor: '.gallery__slider-lg',
    });
@@ -56,7 +56,6 @@ $(function(){
       nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-next.svg" alt="next"></button>',
       slidesToShow: 1,
       slidesToScroll: 1,
-      fade: true,
       arrows: false,
       asNavFor: '.gallery__slider-sm',
       responsive: [
@@ -70,20 +69,18 @@ $(function(){
       ]
    });
 
+   $('.gift__slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+   });
+
    $('.menu__btn').on('click', function () {
-      $('.menu__btn').toggleClass('menu__btn--active');
       $('.menu__list').toggleClass('menu__list--active')
    });
-
-   $('.menu__list-link').on('click', function () {
-      $('.menu__btn').removeClass('menu__btn--active');
-      $('.menu__list').removeClass('menu__list--active');
-   });
-
-
-   // $('.menu__btn').on('click', function () {
-   //    $('.menu__list').toggleClass('menu__list--active')
-   // });
 
    $('.territory__item-link').on('click', function () {
       $('.territory__item-descr').toggleClass('territory__item-descr--active')
@@ -94,7 +91,6 @@ $(function(){
    //    $('.header__list').toggleClass('active');
 
    // });
-   new WOW().init();
 
   
 });
